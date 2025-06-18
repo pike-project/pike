1d9775992ace401303325f285b449f8a486f6980
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2025-06-18 11:48:28 -0700

    flesh out how DiskChannel should be implemented

--- src/util/disk_channel.py
+++ src/util/disk_channel.py
@@ -1,6 +1,15 @@
 import asyncio
 from pathlib import Path
 
+# The DiskChannel uses async file I/O methods to do the following
+# (without watchdog or other deps since this is used on an NFS system)
+# - recv() waits until there is a new "done" flag file in rx_dir, indicating the data is ready
+#   - the "done" flag only indicates the json data file is ready, but the actual data will be in the corresponding json file
+#   - if many new "done" files appear between subsequent checks of the directory, only return one of the corresponding
+#     data files on this iteration, but be sure to return the other data files on future iterations
+# - send() first writes the actual json data to a file, then writes the "done" flag file only when writing of the data is complete
+#   - each data/done pair should have a unique ID, so that outgoing/incoming data files do not clash
+
 class DiskChannel:
     def __init__(self, tx_dir: Path, rx_dir: Path):
         self.tx_dir = tx_dir
@@ -10,8 +19,11 @@ class DiskChannel:
     # TODO: could add timeout parameter, and other utilities, but this is
     # the barebones that we need
     async def recv(self):
-        await asyncio.sleep(10)
-        return {}
+        while True:
+            # this should eventually return when a new "done" file appears in rx_dir
+            await asyncio.sleep(1)
+        # return {}
 
-    def send(self):
+    # TODO: send out data via tx_dir
+    def send(self, data: dict):
         pass

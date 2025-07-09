bf5b5e99232b86f7bd1447b1f22c7c494c8c4a43
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2025-07-09 13:55:34 -0700

    pass hidden layer into level 3 RNN tasks

--- KernelBench/level3/34_VanillaRNNHidden.py
+++ KernelBench/level3/34_VanillaRNNHidden.py
@@ -14,14 +14,13 @@ class Model(nn.Module):
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
-        self.hidden = torch.randn((batch_size, hidden_size))
         
         # Define the RNN cell components (input to hidden, hidden to hidden, and hidden to output)
         self.i2h = nn.Linear(input_size + hidden_size, hidden_size)  # Input to hidden
         self.h2o = nn.Linear(hidden_size, output_size)  # Hidden to output
         self.tanh = nn.Tanh()  # Activation function for hidden state
     
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, hidden: torch.Tensor) -> torch.Tensor:
         """
         Forward pass of the Vanilla RNN.
         
@@ -29,20 +28,20 @@ class Model(nn.Module):
         :param hidden: Hidden state tensor of shape (batch_size, hidden_size).
         :return: Output tensor of shape (batch_size, output_size), and the new hidden state.
         """
-        self.hidden = self.hidden.to(x.device)
-        combined = torch.cat((x, self.hidden), dim=1)  # Concatenate input and hidden state
-        self.hidden = self.tanh(self.i2h(combined))  # Update hidden state
-        output = self.h2o(self.hidden)  # Compute output
-        return self.hidden
+        combined = torch.cat((x, hidden), dim=1)  # Concatenate input and hidden state
+        new_hidden = self.tanh(self.i2h(combined))  # Update hidden state
+        output = self.h2o(new_hidden)  # Compute output
+        return new_hidden
 
-batch_size = 8
+# increased from 8 to 1024
+batch_size = 1024
 input_size = 1024
 hidden_size = 256
 output_size = 128
 sequence_length = 256
 
 def get_inputs():
-    return [torch.randn(batch_size, input_size)]
+    return [torch.randn(batch_size, input_size), torch.randn((batch_size, hidden_size))]
 
 def get_init_inputs():
     return [input_size, hidden_size, output_size]

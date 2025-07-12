2cfc72743f5b7089998c1ee76799b858ee438c91
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2025-07-12 13:45:24 -0700

    increase task range parameter for run script

--- tools/run.sh
+++ tools/run.sh
@@ -1,6 +1,12 @@
 set -ex
 
 LEVEL=0
+# inclusive range
+TASK_START=1
+TASK_END=6
+
+# True or False
+DRY_RUN=True
 
 DATA_DIR=$PSCRATCH/llm/KernelBench-data
 RUN_NAME=$(date +"%Y_%m_%d_%H_%M_%S")
@@ -12,7 +18,7 @@ LOG_PATH=$RUN_DIR/out.log
 WORKER_INPUT_DIR=worker_io/input
 WORKER_OUTPUT_DIR=worker_io/output
 
-python scripts/parallel_tree_search.py data_dir=$DATA_DIR run_dir=$RUN_DIR dataset_src=local server_type=google model_name=gemini-2.5-pro num_workers=50 worker_input_dir=$WORKER_INPUT_DIR worker_output_dir=$WORKER_OUTPUT_DIR level=$LEVEL task_start=1 task_end=1 num_samples=10 dry_run=True | tee $LOG_PATH
+python scripts/parallel_tree_search.py data_dir=$DATA_DIR run_dir=$RUN_DIR dataset_src=local server_type=google model_name=gemini-2.5-pro num_workers=50 worker_input_dir=$WORKER_INPUT_DIR worker_output_dir=$WORKER_OUTPUT_DIR level=$LEVEL task_start=$TASK_START task_end=$TASK_END num_samples=10 dry_run=$DRY_RUN | tee $LOG_PATH
 
 python scripts/solution_eval/eval_solutions.py --level $LEVEL --mode eager --run_dir $RUN_DIR --worker_input_dir $WORKER_INPUT_DIR --worker_output_dir $WORKER_OUTPUT_DIR | tee $LOG_PATH
 python scripts/solution_eval/eval_solutions.py --level $LEVEL --mode compile --run_dir $RUN_DIR --worker_input_dir $WORKER_INPUT_DIR --worker_output_dir $WORKER_OUTPUT_DIR | tee $LOG_PATH

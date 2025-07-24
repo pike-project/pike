f965c33aef15869b9c96a46de5c1649b3f865eba
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2025-07-24 13:22:10 -0700

    make python invocations in run.sh unbuffered

--- tools/run.sh
+++ tools/run.sh
@@ -29,7 +29,8 @@ LOG_PATH=$RUN_DIR/out.log
 WORKER_INPUT_DIR=worker_io/input
 WORKER_OUTPUT_DIR=worker_io/output
 
-python scripts/parallel_tree_search.py data_dir=$DATA_DIR run_dir=$RUN_DIR dataset_src=local server_type=$SERVER_TYPE model_name=$MODEL_NAME num_workers=50 worker_input_dir=$WORKER_INPUT_DIR worker_output_dir=$WORKER_OUTPUT_DIR level=$LEVEL task_start=$TASK_START task_end=$TASK_END num_samples=$NUM_SAMPLES num_phases=$NUM_PHASES max_fix_attempts=$MAX_FIX_ATTEMPTS dry_run=$DRY_RUN | tee -a $LOG_PATH
+# "python -u" makes output unbuffered, so we can see it immediately
+python -u scripts/parallel_tree_search.py data_dir=$DATA_DIR run_dir=$RUN_DIR dataset_src=local server_type=$SERVER_TYPE model_name=$MODEL_NAME num_workers=50 worker_input_dir=$WORKER_INPUT_DIR worker_output_dir=$WORKER_OUTPUT_DIR level=$LEVEL task_start=$TASK_START task_end=$TASK_END num_samples=$NUM_SAMPLES num_phases=$NUM_PHASES max_fix_attempts=$MAX_FIX_ATTEMPTS dry_run=$DRY_RUN | tee -a $LOG_PATH
 
 if [ "$DRY_RUN" = "True" ]; then
     DRY_RUN_FLAG="--dry_run"
@@ -37,7 +38,7 @@ else
     DRY_RUN_FLAG=""
 fi
 
-python scripts/solution_eval/eval_solutions.py --level $LEVEL --mode eager --run_name baseline_eager --run_dir $RUN_DIR --worker_input_dir $WORKER_INPUT_DIR --worker_output_dir $WORKER_OUTPUT_DIR $DRY_RUN_FLAG | tee -a $LOG_PATH
-python scripts/solution_eval/eval_solutions.py --level $LEVEL --mode compile --run_name baseline_compile --run_dir $RUN_DIR --worker_input_dir $WORKER_INPUT_DIR --worker_output_dir $WORKER_OUTPUT_DIR $DRY_RUN_FLAG | tee -a $LOG_PATH
+python -u scripts/solution_eval/eval_solutions.py --level $LEVEL --mode eager --run_name baseline_eager --run_dir $RUN_DIR --worker_input_dir $WORKER_INPUT_DIR --worker_output_dir $WORKER_OUTPUT_DIR $DRY_RUN_FLAG | tee -a $LOG_PATH
+python -u scripts/solution_eval/eval_solutions.py --level $LEVEL --mode compile --run_name baseline_compile --run_dir $RUN_DIR --worker_input_dir $WORKER_INPUT_DIR --worker_output_dir $WORKER_OUTPUT_DIR $DRY_RUN_FLAG | tee -a $LOG_PATH
 
-python scripts/analyze/plot_phase_perf_improvement.py --run_dir $RUN_DIR | tee -a $LOG_PATH
+python -u scripts/analyze/plot_phase_perf_improvement.py --run_dir $RUN_DIR | tee -a $LOG_PATH

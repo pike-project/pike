6a67a1ca5a0d017b3729a41c7a679d0ef3863b42
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2025-08-07 11:45:10 -0700

    make perlmutter job script use srun

--- tools/jobs/perlmutter.sh
+++ tools/jobs/perlmutter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -u sandbox/tools/start_worker_container.py --engine podman-hpc --arch Ampere
+srun -N 1 -A m4141_g -t 24:00:00 -q regular -C 'gpu&hbm40g' --pty python -u sandbox/tools/start_worker_container.py --engine podman-hpc --arch Ampere

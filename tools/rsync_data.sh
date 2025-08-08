6e6d8a1e13a9d850341dc290dc4a5695efc285bb
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2025-08-08 10:19:54 -0700

    remove --delete flag in rsync_data script

--- tools/rsync_data.sh
+++ tools/rsync_data.sh
@@ -5,5 +5,5 @@ DST2=../KernelBench-data/lrc/
 mkdir -p $DST1
 mkdir -p $DST2
 
-rsync -avz --delete perlmutter:/pscratch/sd/k/kir/llm/KernelBench/data/ $DST1
-rsync -avz --delete lrc-xfer:/global/scratch/users/knagaitsev/KernelBench/data/ $DST2
+rsync -avz perlmutter:/pscratch/sd/k/kir/llm/KernelBench/data/ $DST1
+rsync -avz lrc-xfer:/global/scratch/users/knagaitsev/KernelBench/data/ $DST2

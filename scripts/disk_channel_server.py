b7223eb934348bb2f9ee6e44359aa0ba0e6d9b11
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2025-08-22 13:28:46 -0700

    add task check and recv loop in manager

--- scripts/disk_channel_server.py
+++ scripts/disk_channel_server.py
@@ -4,13 +4,13 @@ from pathlib import Path
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from src.util.disk_channel import DiskChannel
 from functools import partial
-from urllib.parse import urlparse, parse_qs
+from urllib.parse import urlparse, parse_qs, quote, unquote
 
 class DiskChannelManager:
     def __init__(self, tx_dir: Path, rx_dir: Path):
         self.disk_channel = DiskChannel(tx_dir, rx_dir)
 
-        self.pending_tasks = {}
+        self.completed_tasks = {}
 
     async def submit(self, code, problem_id, level):
         eval_id = uuid.uuid4()
@@ -26,6 +26,16 @@ class DiskChannelManager:
         #     "mode": "eager"
         # })
 
+        return eval_id
+
+    async def recv_loop(self):
+        while True:
+            msg = await self.disk_channel.recv()
+            self.completed_tasks[msg["id"]] = msg
+
+    def check_task(self, eval_id):
+        return self.completed_tasks.get(eval_id)
+
 class CustomHandler(BaseHTTPRequestHandler):
     def __init__(self, *args, manager: DiskChannelManager, **kwargs):
         self.manager = manager
@@ -39,12 +49,24 @@ class CustomHandler(BaseHTTPRequestHandler):
         path = parsed_url.path
         query_params = parse_qs(parsed_url.query)
 
+        print(query_params)
+
         if path == "/submit":
+            try:
+                code = unquote(query_params.get("code")[0])
+                problem_id = int(query_params.get("problem_id")[0])
+                level = int(query_params.get("level")[0])
+            except Exception as e:
+                self.send_response(500)
+                self.end_headers()
+                self.wfile.write(b"Bad input")
+                return
+
+            eval_id = asyncio.run(manager.submit(code, problem_id, level))
+
             self.send_response(200)
             self.end_headers()
-            self.wfile.write(b"Welcome to the root!")
-
-            asyncio.run(manager.submit("abc", 1, 1))
+            self.wfile.write(f"{eval_id}".encode())
 
         elif path == "/hello":
             self.send_response(200)

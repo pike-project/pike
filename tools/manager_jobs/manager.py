a6377e462d8937a26ca44c0c918cd432f0661429
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2025-09-12 14:22:58 -0700

    get the manager mostly set up

--- tools/manager_jobs/manager.py
+++ tools/manager_jobs/manager.py
@@ -1,6 +1,10 @@
 import os
 import subprocess
 from pathlib import Path
+from time import sleep
+from datetime import datetime
+
+curr_dir = Path(os.path.realpath(os.path.dirname(__file__)))
 
 # Need to run: python scripts/disk_channel_server.py --port 8000
 
@@ -14,9 +18,51 @@ from pathlib import Path
 def main():
     port = 8000
 
-    disk_channel_server = subprocess.Popen(["python", "scripts/disk_channel_server.py", "--port", str(port)])
+    disk_channel_server = subprocess.Popen(
+        ["python", "scripts/disk_channel_server.py", "--port", str(port)],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
 
-    
+    openevolve_dir = Path("/pscratch/sd/k/kir/llm/openevolve")
+
+    example_dir = openevolve_dir / "examples/kernelbench"
+
+    timestamp_str = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    run_dir = example_dir / "openevolve_output_runs" / timestamp_str
+    os.makedirs(run_dir, exist_ok=True)
+
+    root_dir = (curr_dir / "../..").resolve()
+
+    level = "3-metr"
+    task_start = 1
+    task_end = 1
+
+    run_cmd = [
+        "python",
+        "examples/kernelbench/run.py",
+        "--kernel_bench_dir",
+        str(root_dir),
+        "--level",
+        level,
+        "--task_start",
+        task_start,
+        "--task_end",
+        task_end,
+        "--eval_port",
+        str(port),
+        "--run_dir",
+        run_dir,
+    ],
+
+    run = subprocess.Popen(
+        run_cmd,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+        cwd=openevolve_dir,
+    )
+
+    run.wait()
 
     disk_channel_server.terminate()
     disk_channel_server.wait()

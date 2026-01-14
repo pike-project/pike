13e8bc3a3f38de7079977c1e11a5c1924123889d
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2026-01-14 09:14:43 -0600

    target new set of tasks for o3-mini

--- tools/manager/search_manager.py
+++ tools/manager/search_manager.py
@@ -242,7 +242,7 @@ class SearchManager:
 
         tasks = sorted(tasks)
 
-        tasks = [17]
+        tasks = [13, 25, 31, 49]
         print(f"Running these tasks: {tasks}")
 
         return split_into_ranges(tasks, range_count)

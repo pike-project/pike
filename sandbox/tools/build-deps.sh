466d29808e923c7b4997cff19c41978e37cbb96f
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2026-03-06 21:59:15 -0600

    make docker image build

--- sandbox/tools/build-deps.sh
+++ sandbox/tools/build-deps.sh
@@ -1,2 +1,5 @@
-podman-hpc build -f sandbox/Dockerfile.deps -t kernel-bench-deps .
-podman-hpc migrate kernel-bench-deps
+#!/bin/bash
+
+docker build -f sandbox/Dockerfile.deps -t kernel-bench-deps .
+# podman-hpc build -f sandbox/Dockerfile.deps -t kernel-bench-deps .
+# podman-hpc migrate kernel-bench-deps
